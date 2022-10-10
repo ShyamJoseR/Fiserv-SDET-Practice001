aee5135bd458b565ad0f18c6320c6ed6ed556405
Shyam Jose R <shyamjose.r@gmail.com>
2022-10-10 06:53:04 +0530

    Cloned the repository into different folder and made changes to input_num.

--- Fiserv-SDET-GIT-Practive001/src/test/java/org/example/GIT_Odd_Even_Fixed.java
+++ Fiserv-SDET-GIT-Practive001/src/test/java/org/example/GIT_Odd_Even_Fixed.java
@@ -3,6 +3,7 @@ package org.example;
 public class GIT_Odd_Even_Fixed {
     public static void main(String[] args) {
         int input_num = 7;
+        input_num = 8; //override the data after cloning from github
         if(input_num % 2 == 0)
             System.out.println("The input number " + input_num + " is Even...");
         else
