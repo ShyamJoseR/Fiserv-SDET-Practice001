25e17f52ac72e17aafa23a6708d23a0f2a742492
Shyam Jose R <shyamjose.r@gmail.com>
2022-10-10 06:41:14 +0530

    Main Branch program - Odd  & Even number check added to git.

---
+++ Fiserv-SDET-GIT-Practive001/src/test/java/org/example/GIT_Odd_Even_Fixed.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class GIT_Odd_Even_Fixed {
+    public static void main(String[] args) {
+        int input_num = 7;
+        if(input_num % 2 == 0)
+            System.out.println("The input number " + input_num + " is Even...");
+        else
+            System.out.println("The input number " + input_num + " is Odd...");
+    }
+}
